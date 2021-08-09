55400d46d80a656343db77471c1cfd0cf3fd03d2
blackplanthom <blackplanthom87@protonmail.com>
2021-08-09 19:08:01 -0300

    initial commit

---
+++ src/help.js
@@ -0,0 +1,216 @@
+const help = (prefix, emoji) => {
+	return `
+<══════════════════════>
+      *BOT DO ꪶ𝐉𝐀𝐏𝐀 777ꫂ*
+ <══════════════════════>
+
+ ● ᏴϴͲ●ᎷᎬΝႮ●         
+
+
+┌─────────❶.❷────────
+╠══NOTA »
+║esse bot tem comandos infinitos 
+║se o comando não for registrado ele faz uma pesquisa
+╠════════════════════
+║〘 INFORMAÇÕES 〙
+║
+║${emoji} *${prefix}figu*
+║${emoji} *${prefix}toimg*
+║${emoji} *${prefix}darkjokes (memes aleatórios)*
+║${emoji} *${prefix}memeindo*
+║${emoji} *${prefix}tts*
+║${emoji} *${prefix}lolih [on]*
+║${emoji} *${prefix}nsfwloli [off]*
+║${emoji} *${prefix}url2img*
+║${emoji} *${prefix}leens [na legenda]*
+║${emoji} *${prefix}wait [na legenda]*
+║${emoji} *${prefix}setprefix*
+║
+╠══🌸〘 OUTROS 〙🌸══
+║
+║${emoji} *${prefix}linkgp*
+║${emoji} *${prefix}simih [1/0]*
+║${emoji} *${prefix}marcar*
+║${emoji} *${prefix}add [@]*
+║${emoji} *${prefix}banir [@]*
+║${emoji} *${prefix}promover [@]*
+║${emoji} *${prefix}rebaixar*
+║${emoji} *${prefix}admins*
+║${emoji} *${prefix}marcar2*
+║${emoji} *${prefix}bc [texto]* (ele faz uma ™)
+║${emoji} *${prefix}marcar3*
+║${emoji} *${prefix}bloqueados*
+║${emoji} *${prefix}bloquear [@]*
+║${emoji} *${prefix}desbloquear [@]*
+║${emoji} *${prefix}limpar*
+║${emoji} *${prefix}bc [ *texto* ]*
+║${emoji} *${prefix}bemvindo [1/0]*
+║${emoji} *${prefix}clonar [@]*
+║${emoji} *${prefix}help1*
+║${emoji} *${prefix}dono*
+║${emoji} *${prefix}owner*
+║${emoji} *${prefix}tts [texto]*
+║${emoji} *${prefix}setnome*
+║${emoji} *${prefix}termux*
+║${emoji} *${prefix}setfoto*
+║${emoji} *${prefix}grupoinfo*
+║${emoji} *${prefix}ytmp4*
+║${emoji} *${prefix}bomdia*
+║${emoji} *${prefix}boanoite*
+║${emoji} *${prefix}marcar*
+║${emoji} *${prefix}marcar2*
+║${emoji} *${prefix}marcar3*
+║
+╠══🌸〘 IMAGENS 〙🌸══
+║
+║${emoji} *${prefix}loli* [off]
+║${emoji} *${prefix}loli1*
+║${emoji} *${prefix}dono*
+║${emoji} *${prefix}boanoite*
+║${emoji} *${prefix}bomdia*
+║${emoji} *${prefix}boatarde*
+║${emoji} *${prefix}mia [aleatórias]*
+║${emoji} *${prefix}rize [aleatórias]*
+║${emoji} *${prefix}minato [aleatórias]*
+║${emoji} *${prefix}boruto [aleatórias]*
+║${emoji} *${prefix}hinata [aleatórias]*
+║${emoji} *${prefix}sasuke [aleatórias]*
+║${emoji} *${prefix}sakura [aleatórias]*
+║${emoji} *${prefix}naruto [aleatórias]*
+║${emoji} *${prefix}meme*   
+║${emoji} *${prefix}lofi*
+║${emoji} *${prefix}canal*
+║${emoji} *${prefix}nsfwloli1*
+║${emoji} *${prefix}reislin*
+║
+╠══🌸〘 INTELIGÊNCIA 〙🌸══
+║
+║${emoji} *${prefix}simih 1 (para ativar)*
+║${emoji} *${prefix}simih 0 (para desativar)*
+║ *${prefix}simi (sua mensagem)*
+║
+╠══🌸〘 SÓ PREMIUM 〙🌸══
+╠══NOTA »
+║esse 777 é o cão msm 😳
+╠════════════════════
+║${emoji} *${prefix}dado*
+║${emoji} *${prefix}cekvip*
+║${emoji} *${prefix}premiumlist*
+║${emoji} *${prefix}delete*
+║${emoji} *${prefix}modapk*
+║${emoji} *${prefix}indo10*
+║${emoji} *${prefix}daftarvip [para virar Premium]*
+║${emoji} *${prefix}qrcode*
+║${emoji} *${prefix}chentai*
+║${emoji} *${prefix}gcpf*
+║${emoji} *${prefix}gbin*
+║${emoji} *${prefix}pack*
+║${emoji} *${prefix}destrava*
+║${emoji} *${prefix}gpessoa*
+║
+╠══🌸〘 GRUPO 〙🌸══
+║
+║${emoji} *${prefix}banir*
+║${emoji} *${prefix}leveling [on/off]*
+║${emoji} *${prefix}level*
+║${emoji} *${prefix}add*
+║${emoji} *${prefix}promover*
+║${emoji} *${prefix}setfoto [na legenda]*
+║${emoji} *${prefix}setname [texto]*
+║${emoji} *${prefix}rebaixar*
+║${emoji} *${prefix}admins*
+║${emoji} *${prefix}marcar*
+║${emoji} *${prefix}marcar2*
+║${emoji} *${prefix}marcar3*
+║${emoji} *${prefix}bemvindo [1/0]*
+║${emoji} *${prefix}grupoinfo*
+║${emoji} *${prefix}bomdia*
+║${emoji} *${prefix}boatarde*
+║${emoji} *${prefix}boanoite*
+║${emoji} *${prefix}setdesc*
+║
+╠══🌸〘 ESPECIFICO DO BOT 〙🌸══
+║
+║${emoji} *${prefix}clonar [@]*
+║${emoji} *${prefix}dono*
+║${emoji} *${prefix}ping [ver velocidade do bot]*
+║${emoji} *${prefix}termux*
+║${emoji} *${prefix}gay [@]*
+║${emoji} *${prefix}wame*
+║${emoji} *${prefix}map (nome)*
+║${emoji} *${prefix}setppbot (marque uma img)*
+║${emoji} *${prefix}pinterest (nome)*
+║${emoji} *${prefix}desligar (so para o dono)*
+║${emoji} *${prefix}timer*
+║
+╠══🌸〘 MAIS ALGUNS 〙🌸══
+║
+║${emoji} *${prefix}neko*
+║${emoji} *${prefix}ttp [texto]*
+║${emoji} *${prefix}testime*
+║${emoji} *${prefix}tomp3*
+║${emoji} *${prefix}modoanime [on/off]*
+║${emoji} *${prefix}modonsfw [on/off]*
+║${emoji} *${prefix}happymod [jogo/app]*
+║${emoji} *${prefix}rize*
+║${emoji} *${prefix}ytsearch*
+║${emoji} *${prefix}moddroid [jogo/app]*
+║${emoji} *${prefix}nomegp*
+║${emoji} *${prefix}darkjokes (memes aleatórios)*
+║${emoji} *${prefix}animecry*
+║${emoji} *${prefix}gay1*
+║${emoji} *${prefix}next*
+║${emoji} *${prefix}alerta*
+║${emoji} *${prefix}belle [img aleatórias]*
+║${emoji} *${prefix}pronomeneu [texto]*
+║${emoji} *${prefix}hobby*
+║${emoji} *${prefix}kiss
+║${emoji} *${prefix}analise
+║
+╠══🌸〘 COMANDOS DE VOZ 〙🌸══
+║
+║${emoji} *${prefix}ola*
+║${emoji} *${prefix}bv*
+║${emoji} *${prefix}tchau*
+║${emoji} *${prefix}bem*
+║${emoji} *${prefix}a*
+║${emoji} *${prefix}fdp*
+║${emoji} *${prefix}onich*
+║${emoji} *${prefix}beat1*
+║${emoji} *${prefix}glub*
+║${emoji} *${prefix}causs*
+║
+╠══🌸〘 OUTROS /2 〙🌸══
+║
+║${emoji} *${prefix}antilink [1/0]*
+║${emoji} *${prefix}brainly [pergunta]*
+║${emoji} *${prefix}antiracismo [on/off]*
+║${emoji} *${prefix}setnomebot*
+║${emoji} *${prefix}meme*
+║
+╠══🌸〘 INTERATIVOS 〙🌸══
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║${emoji} *bah*
+║${emoji} *oii*
+║${emoji} *bv*
+║${emoji} *canta ai bot*
+║${emoji} *grita*
+║${emoji} *gemidao*
+║${emoji} *musica*
+║
+╠══🌸〘 CRÉDITOS 〙🌸══
+║
+║ *NOME: 🥀ꪶ𝐉𝐀𝐏𝐀 777ꫂ🥀*
+║ *INSTA: japakkkkkkk*
+║ *YOUTUBE: https://youtube.com/c/JAPA777xs*
+║
+║
+╚═〘 *BOT DO 777* 〙`
+}
+
+exports.help = help
+
